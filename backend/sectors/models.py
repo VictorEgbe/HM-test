374434a01c307b8c2dc7861020c7cc64260e456a
unknown <victoregbejunior@gmail.com>
2023-12-15 15:24:16 +0100

    Created backend models for sectors

--- backend/sectors/models.py
+++ backend/sectors/models.py
@@ -1,5 +1,31 @@
 from django.db import models
 
 
+class User(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    agreed = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class SectorHeading(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Sector(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sector_name = models.CharField(max_length=150)
+    heading = models.ForeignKey(SectorHeading, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.sector_name
